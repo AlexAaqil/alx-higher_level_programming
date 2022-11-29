2aa03ec76ceb50e87a7559d6b02479b302a12956
AlexAaqil <alexdequantes@gmail.com>
2022-11-29 12:51:45 -0800

    check for positive or negative numbers

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+if number > 0:
+    print(f"{number} is positive", number);
+elif number == 0:
+    print(f"{number} is zero", number);
+elif number < 0:
+    printf(f"{number} is negative", number);
+print()
+
